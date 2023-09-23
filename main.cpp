04ac67e4f6773d932d7f9be22afe69e746c7bc10
Maria Soloveva <mariasoloveva2476@gmail.com>
2023-09-23 23:25:40 +0300

    lab_1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
 
 #include "lib.h"
 
 using namespace std;
+                                                                                                                   
 
-int main() {
-    cout << "Bruh: " << endl
-         << A() << endl;
+int main(){
+    int hour, minute;
+    string period;
+
+    cout << "Enter hour(1 - 12): ";
+    cin >> hour;
+
+    cout << "Enter minute(0 - 59): ";
+    cin >> minute; 
+
+    cout << "Enter period(am/pm): ";
+    cin >> period; 
+
+    string ConvertTime = ConvertTo24h(hour, minute, period);
+
+    cout << "Convert to 24-hour format time: " <<  ConvertTime << endl;
+    
+    return 0;
 }
